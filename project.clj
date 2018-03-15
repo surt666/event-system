08b7667ce5bc5cbd9426ad02a3b1fa25cd561bce
Steen Larsen <sl@skm.dk>
2018-03-15 15:58:44 +0100

    command added 2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-system "0.10.0"
+(defproject event-system "0.11.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
