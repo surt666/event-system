5ba6eb6be1c4abed738f18fc701181677afca8c2
Steen Larsen <sl@skm.dk>
2018-03-15 10:35:03 +0100

    change function name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-system "0.4.0"
+(defproject event-system "0.5.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
