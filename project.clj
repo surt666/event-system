835191d8b70f9f7145dde2d84bbace7448b724c1
Steen Larsen <sl@skm.dk>
2018-03-15 18:28:58 +0100

    fixed event

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-system "0.11.0"
+(defproject event-system "0.12.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
